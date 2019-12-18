9e21d866b5e4bc4c9244ae3113cd3a36e4c20c41
vedanshvachani <vedanshvachani16@gmail.com>
2019-12-18 16:37:51 +0530

    Add files via upload

---
+++ clientPassword.c
@@ -0,0 +1,36 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<netdb.h>
+int main(){
+	int client;
+	struct sockaddr_in saddr;
+
+	saddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	saddr.sin_family=AF_INET;
+	saddr.sin_port=3012;
+	
+	client=socket(PF_INET,SOCK_STREAM,0);
+	connect(client,(struct sockaddr *)&saddr,sizeof(saddr));
+
+	char d1[30], d2[30],d3[30];
+	int i;
+	int random=rand();
+	sprintf(d3,"%d",random+5);
+	send(client,d3,30,0);
+	printf("Enter the Password");
+	scanf("%s",d1);
+	for (i=0; d1[i]!='\0'; i++){
+		d1[i]=d1[i]+random;
+	}
+	send(client,d1,30,0);
+		
+	recv(client,d2,30,0);
+	
+	for(i=0; d2[i]!='\0'; i++){
+		d2[i]=d2[i]-1;
+	}
+	
+	printf("\nresult: %s ",d2);
+}
---
+++ serverPassword.c
@@ -0,0 +1,47 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<netdb.h>
+int main(){
+	int client,server;
+	//2 structures of socket that is defined in socket.h
+	struct sockaddr_in saddr,caddr;
+
+	char pass[] = "1234"; //password to crosscheck
+	
+	//ip-address
+	saddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	
+	saddr.sin_family=AF_INET;
+	//port number
+	saddr.sin_port=3012;
+	
+	//socket connection for tcp
+	server=socket(PF_INET,SOCK_STREAM,0);
+	bind(server,(struct sockaddr *)&saddr,sizeof(saddr));
+
+	listen(server,5);
+	char d1[30], d2[30],d3[30];
+	int i,random;
+	int len=sizeof(caddr);
+	while(1){
+		client=accept(server,(struct sockaddr*)&caddr,&len);
+		recv(client,d3,30,0);
+		random=(atoi(d3)-5);
+		recv(client,d1,30,0);
+		for (i=0; d1[i]!='\0'; i++){
+			d1[i]=d1[i]-random;
+		}
+		if(strcmp(pass,d1) == 0){
+			for(i=0; d1[i]!='\0'; i++){
+				d1[i]=d1[i]+1;
+			}
+			send(client,d1,30,0);
+		}
+		else{
+			sprintf(d1,"Wrong password");
+			send(client,d1,30,0);
+		}
+	}	
+}
